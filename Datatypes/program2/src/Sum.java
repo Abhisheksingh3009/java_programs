67ee0b8319a48b37afaf4798c4be17e72912964f
Abhishek <abhishek.singh7586@gmail.com>
2019-01-12 21:25:00 +0530

    commit tree

---
+++ Datatypes/program2/src/Sum.java
@@ -0,0 +1,8 @@
+class Sum{
+	public static void main(String args[]){
+		int num1=20,num2=100;
+		System.out.println("Addition of two integers num1 and num2 are:");
+		System.out.println(num1+num2);
+		
+	}
+}
---
+++ Datatypes/program3/src/AsciiValue.java
@@ -0,0 +1,11 @@
+class AsciiValue
+{
+	public static void main(String args[]){
+	
+		int i='D';
+		System.out.println("Ascii Value of given character is:");
+		System.out.println(i);
+		
+	}
+
+}
